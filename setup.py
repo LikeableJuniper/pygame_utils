1dc846855d23b8fb122d7b21a8ba7519d04e30b6
Steffen Noa (IT-PTR-CEN2-BDE23) <noa.steffen@sbb.ch>
2026-07-15 13:15:29 +0200

    added base setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="pygame_utils_likeablejuniper",
+    version="0.1.0",
+    description="Package which provides some basic GUI elements for pygame",
+    url="https://github.com/LikeableJuniper/pygame_utils"
+)
