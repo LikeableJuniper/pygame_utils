70840b7769206d6c35d4b8b045e79e63ed3c5b9b
Steffen Noa (IT-PTR-CEN2-BDE23) <noa.steffen@sbb.ch>
2026-07-15 16:37:28 +0200

    basic example file

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import pygame as pg
 import pygame_utils_likeablejuniper as pgu
 
-print(pgu.__version__)
+pg.init()
+screen = pg.display.set_mode((800, 800))
+
+running = True
+
+while running:
+    screen.fill((100, 100, 100))
+
+    pg.display.flip()
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+pg.quit()
