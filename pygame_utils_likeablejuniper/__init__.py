f93b18fe6d4e80357fb53e7458cad3f9f0777be1
Steffen Noa (IT-PTR-CEN2-BDE23) <noa.steffen@sbb.ch>
2026-07-21 15:39:52 +0200

    refactoring in preparation for wiki

--- pygame_utils_likeablejuniper/__init__.py
+++ pygame_utils_likeablejuniper/__init__.py
@@ -4,9 +4,9 @@ pg.font.init()
 
 from pygame_utils_likeablejuniper.style.style import Border
 from pygame_utils_likeablejuniper.style.theme import Theme, Themes
-from pygame_utils_likeablejuniper.core.container import Container, ContainerStyle
-from pygame_utils_likeablejuniper.component.label import Label, StaticLabel, LabelStyle
-from pygame_utils_likeablejuniper.component.button import Button, ButtonStyle
+from pygame_utils_likeablejuniper.element.container import Container, ContainerStyle
+from pygame_utils_likeablejuniper.element.label import Label, StaticLabel, LabelStyle
+from pygame_utils_likeablejuniper.element.button import Button, ButtonStyle
 
 __version__ = "0.2.0"
 __author__ = "LikeableJuniper"
