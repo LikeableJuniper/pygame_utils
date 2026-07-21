b0d2b2529b3d9ebe4eac6127cf994ea456f4d704
Steffen Noa (IT-PTR-CEN2-BDE23) <noa.steffen@sbb.ch>
2026-07-21 15:43:03 +0200

    shorter import for layout

--- pygame_utils_likeablejuniper/__init__.py
+++ pygame_utils_likeablejuniper/__init__.py
@@ -7,6 +7,7 @@ from pygame_utils_likeablejuniper.style.theme import Theme, Themes
 from pygame_utils_likeablejuniper.element.container import Container, ContainerStyle
 from pygame_utils_likeablejuniper.element.label import Label, StaticLabel, LabelStyle
 from pygame_utils_likeablejuniper.element.button import Button, ButtonStyle
+from pygame_utils_likeablejuniper.layout.layout import VerticalLayout, HorizontalLayout
 
 __version__ = "0.2.0"
 __author__ = "LikeableJuniper"
